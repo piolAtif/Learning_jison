7f48ee0f691b0c1c76ec52b589c7eb6620fbfdc3
piolAtif <spreeti@thoughtworks.com>
2016-11-18 15:37:09 +0530

    [Vathsala/Preeti]  +Modified utils file

--- utils.js
+++ utils.js
@@ -21,23 +21,23 @@ var operatorToWord = function(operator){
 	return operations[operator];
 }
 
-var represent = function(list,delimiters,ConvertFunc){
+var represent = function(list,delimiters,ConvertFunc,operatorFunc){
 	var data = JSON.parse(JSON.stringify(list));
-	data[1] = operatorToWord(data[1]);
+	data[1] = operatorFunc(data[1]);
 	data[2] = ConvertFunc(data[2]);
 	if(!(data[0] instanceof Array)){
 		data[0] = ConvertFunc(+data[0]);
 	}
 	if( data[0] instanceof Array){
-		data[0] = represent(data[0],delimiters,ConvertFunc);
+		data[0] = represent(data[0],delimiters,ConvertFunc,operatorFunc);
 	}
 	return delimiters[0]+data.join(' ')+delimiters[1];
 }
 
 utils.parse = function(){
 
-	console.log(represent(utils.list,["( " , " )"],identity));
-	console.log(represent(utils.list,["( " , " )"],converter.toWords));
+	console.log(represent(utils.list,["( " , " )"],identity,identity));
+	console.log(represent(utils.list,["( " , " )"],converter.toWords,operatorToWord));
 }
 
 module.exports = utils;
