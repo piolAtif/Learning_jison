72992dc07b28215a45077f2ce205dc8931f9b33b
piolAtif <spreeti@thoughtworks.com>
2016-11-21 19:11:52 +0530

    Added grammar to argv

--- generateParser.js
+++ generateParser.js
@@ -1,4 +1,4 @@
-var file = require('fs').readFileSync('./expressMathematicalExpression.jison', 'UTF-8');
+var file = require('fs').readFileSync(process.argv[2], 'UTF-8');
 var Parser = require("jison").Parser;
 var parser = new Parser(file);
 parser.parse("2+3*4");
