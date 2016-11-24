f356cf4e8d2226d13e5797b5fd2ddde93f21cb8a
piolAtif <spreeti@thoughtworks.com>
2016-11-24 10:41:21 +0530

    [Dharam/Preeti] +Added ; at the end of each expression

--- test/treeTest.js
+++ test/treeTest.js
@@ -8,31 +8,31 @@ var parser = new Parser(grammar);
 describe('parseTree', function() {
 	describe('parenthesis', function() {
 		it('should give parenthesis expression of simple plus expression', function() {
-			var tree = parser.parse('1+2');
+			var tree = parser.parse('1+2;');
 			var expected = '( 1 + 2 )';
 			assert.equal(expected, tree.id);
 		});
 
 		it('should give parenthesis expression of simple multiply expression', function() {
-			var tree = parser.parse('1*2');
+			var tree = parser.parse('1*2;');
 			var expected = '( 1 * 2 )';
 			assert.equal(expected, tree.id);
 		});
 
 		it('should give parenthesis expression of simple expression', function() {
-			var tree = parser.parse('1 * 2 + 1');
+			var tree = parser.parse('1 * 2 + 1;');
 			var expected = '( ( 1 * 2 ) + 1 )';
 			assert.equal(expected, tree.id);
 		});
 
 		it('should give parenthesis expression of little complex expression', function() {
-			var tree = parser.parse('2 + 3 * 4');
+			var tree = parser.parse('2 + 3 * 4;');
 			var expected = '( 2 + ( 3 * 4 ) )';
 			assert.equal(expected, tree.id);
 		});
 
 		it('should give parenthesis expression of a complex expression', function() {
-			var tree = parser.parse('1 + 2 + 3 + 4');
+			var tree = parser.parse('1 + 2 + 3 + 4;');
 			var expected = '( ( ( 1 + 2 ) + 3 ) + 4 )';
 			assert.equal(expected, tree.id);
 		});
@@ -40,25 +40,25 @@ describe('parseTree', function() {
 
 	describe('in words', function() {
 		it('should give simple plus expression in words', function(){
-			var tree = parser.parse('1+2');
+			var tree = parser.parse('1+2;');
 			var expected = '( one plus two )';
 			assert.equal(expected, tree.words);
 		})
 
 		it('should give words expression of little complex expression', function() {
-			var tree = parser.parse('2 + 3 * 4');
+			var tree = parser.parse('2 + 3 * 4;');
 			var expected = '( two plus ( three times four ) )';
 			assert.equal(expected, tree.words);
 		});
 
 		it('should give words expression for billion number', function() {
-			var tree = parser.parse('1000000000+2');
+			var tree = parser.parse('1000000000+2;');
 			var expected = '( one billion plus two )';
 			assert.equal(expected, tree.words);
 		});
 
 		it('should give words expression of more complex expression', function() {
-			var tree = parser.parse('1 + 2 + 3 * 4 + 3 - 1');
+			var tree = parser.parse('1 + 2 + 3 * 4 + 3 - 1;');
 			var expected = '( ( ( ( one plus two ) plus ( three times four ) ) plus three ) minus one )';
 			assert.equal(expected, tree.words);
 		});
@@ -67,13 +67,13 @@ describe('parseTree', function() {
 
 	describe('evaluate', function(){
 		
-		it.only('should give 25 after evaluate expression', function(){
+		it('should give 25 after evaluate expression', function(){
 			var tree = parser.parse('x = 5;x*5;');
 			assert.equal(25, tree.eval);
 		});
 
 		it('should give 10 for given expression', function() {
-			var tree = parser.parse('x=1; x + 2 + 3 + 4');
+			var tree = parser.parse('x=1; x + 2 + 3 + 4;');
 			assert.equal(10, tree.eval);
 		});
 
