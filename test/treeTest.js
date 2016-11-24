30196d0e32f6ecb5262bba6bb2958e9789cede7b
piolAtif <spreeti@thoughtworks.com>
2016-11-24 19:59:51 +0530

    [Lalit/Preeti] +Added new tests

--- test/treeTest.js
+++ test/treeTest.js
@@ -10,7 +10,7 @@ describe('parseTree', function() {
 		it('should give parenthesis expression of simple plus expression', function() {
 			var tree = parser.parse('1+2;');
 			var expected = '( 1 + 2 )';
-			assert.equal(expected, tree.withParenthesis());
+			assert.equal(expected, tree.withParenthesis	());
 		});
 
 		it('should give parenthesis expression of simple multiply expression', function() {
@@ -67,29 +67,38 @@ describe('parseTree', function() {
 
 	describe('evaluate', function(){
 		
+		it('should evaluate simple expression', function() {
+			var tree = parser.parse('3+4+4;')
+			assert.equal(11, tree.evaluate())
+		})
 		it('should assign value to x for simple expression', function(){
 			var tree = parser.parse('x = 5;');
-			assert.equal(5, tree.eval());
+			assert.equal(5, tree.evaluate());
 		});
 
 		it('should give 25 for given expression', function(){
-			var tree = parser.parse('x=5; b=5; b*a;');
-			assert.equal(25, tree.eval());
+			var tree = parser.parse('a=5; b=5; b*a;');
+			assert.equal(25, tree.evaluate());
 		});
+
+		it('should give 10 for a complex expression',function(){
+			var tree = parser.parse('a=5; b=a; a+b;');
+			assert.equal(10, tree.evaluate());
+		})
 
 		it('should give 10 for given expression', function() {
 			var tree = parser.parse('x=1; x + 2 + 3 + 4;');
-			assert.equal(10, tree.eval());
+			assert.equal(10, tree.evaluate());
 		});
 
 		it('should give minus 400 for given expression', function(){
 			var tree = parser.parse('x=10;y=20;z=30; x^2+y^2-z^2;');
-			assert.equal(-400,tree.eval());
+			assert.equal(-400,tree.evaluate());
 		});
 
 		it('should give error when evaluate variable before assign value to variable', function(){
 			var tree = parser.parse('x^2; x=10;');
-			assert.equal("x is not defined",tree.eval());
+			assert.equal("x is not defined",tree.evaluate());
 		});
 	});
 	
