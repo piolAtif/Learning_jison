ec6ad3272ab1a228cb344361e9f18e5c2c83b577
piolAtif <spreeti@thoughtworks.com>
2016-11-26 17:41:42 +0530

    Added tests for nodes

--- test/nodesTest.js
+++ test/nodesTest.js
@@ -25,7 +25,7 @@ describe('Evaluate expressions', function() {
     var firstNumberNode = utils.createNumberNode(1);
     var secondNumberNode = utils.createNumberNode(2);
     var plusNode = utils.createPlusNode(firstNumberNode,'+',secondNumberNode);
-    var multiplyNode = utils.createPlusNode(firstNumberNode,'*',secondNumberNode);
+    var multiplyNode = utils.createMultiplyNode(firstNumberNode,'*',secondNumberNode);
 
     describe('Evaluate expressions with Parenthesis', function() {
         describe('should pass string when given expression is a single array', function() {
@@ -35,20 +35,17 @@ describe('Evaluate expressions', function() {
             });
 
             it('should give expression for multiplication of two numbers', function() {
-                var tree = [multiplyNode];
                 var expectedResult = '( 1 * 2 )';
-                assert.equal(utils.parse(tree).id, expectedResult);
+                assert.equal(multiplyNode.withParenthesis(), expectedResult);
             });
         });
 
         describe('should pass string when given expression is array of arrays', function() {
             var thirdNumberNode = utils.createNumberNode(3);
+            var multiplyNode = utils.createPlusNode(plusNode,'*',thirdNumberNode);
             it('should give expression for sum of two numbers multiply by third number', function() {
-                var tree = [
-                    [firstNumberNode, plusNode, secondNumberNode], multiplyNode, thirdNumberNode
-                ];
                 var expectedResult = '( ( 1 + 2 ) * 3 )';
-                assert.equal(utils.parse(tree).id, expectedResult);
+                assert.equal(multiplyNode.withParenthesis(), expectedResult);
             });
         });
 
@@ -56,38 +53,35 @@ describe('Evaluate expressions', function() {
     describe('Evaluate expression in words', function() {
         describe('should express string in word when given expression is a single array', function() {
             it('should give word expression for sum of two numbers', function() {
-                var tree = [firstNumberNode, plusNode, secondNumberNode];
                 var expectedResult = '( one plus two )';
-                assert.equal(utils.parse(tree).words, expectedResult);
+                assert.equal(plusNode.toWords(), expectedResult);
             });
 
             it('should give word expression for multiplication of two numbers', function() {
-                var tree = [firstNumberNode, multiplyNode, secondNumberNode];
                 var expectedResult = '( one times two )';
-                assert.equal(utils.parse(tree).words, expectedResult);
+                assert.equal(multiplyNode.toWords(), expectedResult);
             });
         });
 
         describe('should give word expression when given expression is array of arrays', function() {
-            var thirdNumberNode = utils.createNumberNode(3);
+             var thirdNumberNode = utils.createNumberNode(3);
+            var multiplyNode = utils.createMultiplyNode(plusNode,'*',thirdNumberNode);
             it('should give word expression for sum of two numbers multiply by third number', function() {
-                var tree = [
-                    [firstNumberNode, plusNode, secondNumberNode], multiplyNode, thirdNumberNode
-                ];
                 var expectedResult = '( ( one plus two ) times three )';
-                assert.equal(utils.parse(tree).words, expectedResult);
+                assert.equal(multiplyNode.toWords(), expectedResult);
             });
         });
     });
     describe('Evaluate expression when expressions are in multiple statement',function(){
-        it('should return 25 after evaluating the expression',function(){
+        it('should return 30 after evaluating the expression',function(){
             var variableNode = utils.createVariableNode('x');
-            var assignNode = utils.createAssignNode('=');
             var firstNumberNode = utils.createNumberNode(10);
+            var assignNode = utils.createAssignNode(variableNode,'=',firstNumberNode);
             var secondNumberNode = utils.createNumberNode(5);
+            var plusNode = utils.createPlusNode(assignNode,'+',secondNumberNode)
             var thirdNumberNode = utils.createNumberNode(2);
-            var tree = [[[variableNode, assignNode, firstNumberNode],plusNode,secondNumberNode],multiplyNode,thirdNumberNode];
-            assert.equal(25, utils.parse(tree).eval);
+            var multiplyNode = utils.createMultiplyNode(plusNode,'*', thirdNumberNode);
+            assert.equal(30, multiplyNode.evaluate({'_': undefined})['_']);
         })
     })
 });
